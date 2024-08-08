c137c3461b0132f049dec167127491b3d711ff72
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-08-08 13:50:42 +0100

    deuxieme segmentation des pages

---
+++ include/slider_parte.php
@@ -0,0 +1,28 @@
+<div class="slider_partenaire space_bottom">
+    <div class="slide2 space">
+        <div class="text_confiance">
+            <p>Ils nous font <span class="text_color">confiance</span></p>
+        </div>
+    </div>
+    <div class="parten_all flex space2">
+        <div class="parten_items">
+            <img src="image/enco.png" alt="">
+        </div>
+        <div class="parten_items">
+            <img src="image/rp.png" alt="">
+        </div>
+        <div class="parten_items">
+            <img src="image/gret.png" alt="">
+        </div>
+        <div class="parten_items">
+            <img src="image/ue.png" alt="">
+        </div>
+        <div class="parten_items">
+            <img src="image/banque.png" alt="">
+        </div>
+        <div class="parten_items">
+            <img src="image/bralico.png" alt="">
+        </div>
+    </div>
+
+</div>
---
+++ include/whycote.php
@@ -0,0 +1,82 @@
+<div class="section_presentation flex space2 space_bottom">
+    <div class="section_presentation_left">
+        <div class="section_presnt_lef_title">
+            <p>
+                Pourquoi choisir COTECHCOM?
+            </p>
+        </div>
+        <div class="section_presentation_left_description">
+            <p>COTECHCOM est une société spécialisée dans la Communication, la Technologie et la construction des
+                forages. Engagée dans une dynamique de croissance, elle développe les segments des particuliers et des
+                professionnels, tout en consolidant ses relations avec les entreprises.</p>
+        </div>
+    </div>
+    <div class="section_presentation_right flex choice_all">
+        <div class="flex choice_items">
+            <div class="choicenumber choicenumber1">
+                <span class="ellipse flex">
+                    <p>1</p>
+                </span>
+            </div>
+            <div class="choicetext choicetext1">
+                <div class="choicetextTitle">
+                    <p>Expertise multidisciplinaire </p>
+                </div>
+                <div class="choicetextSub">
+                    <p>Nos équipes spécialisées dans divers secteurs vous garantissent des
+                        solutions intégrées et complètes.</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex choice_items">
+            <div class="choicenumber choicenumber1">
+                <span class="ellipse flex">
+                    <p>1</p>
+                </span>
+            </div>
+            <div class="choicetext choicetext1">
+                <div class="choicetextTitle">
+                    <p>Expertise multidisciplinaire </p>
+                </div>
+                <div class="choicetextSub">
+                    <p>Nos équipes spécialisées dans divers secteurs vous garantissent des
+                        solutions intégrées et complètes.</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex choice_items">
+            <div class="choicenumber choicenumber1">
+                <span class="ellipse flex">
+                    <p>1</p>
+                </span>
+            </div>
+            <div class="choicetext choicetext1">
+                <div class="choicetextTitle">
+                    <p>Expertise multidisciplinaire </p>
+                </div>
+                <div class="choicetextSub">
+                    <p>Nos équipes spécialisées dans divers secteurs vous garantissent des
+                        solutions intégrées et complètes.</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex choice_items">
+            <div class="choicenumber choicenumber1">
+                <span class="ellipse flex">
+                    <p>1</p>
+                </span>
+            </div>
+            <div class="choicetext choicetext1">
+                <div class="choicetextTitle">
+                    <p>Expertise multidisciplinaire </p>
+                </div>
+                <div class="choicetextSub">
+                    <p>Nos équipes spécialisées dans divers secteurs vous garantissent des
+                        solutions intégrées et complètes.</p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
