31e832fcba1313a383533c5c97b39b84cdda9d4b
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-08-08 12:48:10 +0100

    segmentation des pages

---
+++ include/slide_realisation.php
@@ -0,0 +1,91 @@
+<div class="slider_real_bottom space_bottom">
+    <div class="slide2 space">
+        <div class="text_realisation">
+            <p><span class="text_color">Nos</span> réalisations</p>
+        </div>
+        <div class="flex swiper mySwiper space2">
+            <div class="swiper-wrapper flex">
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+                <a href="" class="swiper-slide">
+                    <div class="swiper-slide-text-slide">
+                        <div class="swiper-slide-text-haut">
+                            <p>Réalisation d’un forage d’eau potable à la Likouala</p>
+                        </div>
+                        <div class="swiper-slide-text-bas">
+                            <p>21 janvier 2024</p>
+                        </div>
+                    </div>
+                    <img src="image/slide_real2.jpg" />
+                </a>
+            </div>
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev flex"></div>
+            <div class="swiper-button-next flex"></div>
+        </div>
+    </div>
+</div>
---
+++ include/slide_service.php
@@ -0,0 +1,225 @@
+<!-- Slider main container -->
+<div class="slider_top">
+    <div class="swiper space_top space_bottom">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+                <div class="slide_image">
+                    <img src="image/slide1.jpg" alt="">
+                </div>
+                <div class="slide_filter">
+                    <div class="slide-text">
+                        <h1>Un accès sûr à l'eau</h1>
+                        <h3>Des forages d'eau de qualité supérieure pour un accès ininterrompu à l'eau potable</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="slide_image">
+                    <img src="image/slide2.jpg" alt="">
+                </div>
+                <div class="slide_filter">
+                    <div class="slide-text">
+                        <h1>Un accès sûr à l'eau</h1>
+                        <h3>Des forages d'eau de qualité supérieure pour un accès ininterrompu à l'eau potable</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="slide_image">
+                    <img src="image/slide1.jpg" alt="">
+                </div>
+                <div class="slide_filter">
+                    <div class="slide-text">
+                        <h1>Un accès sûr à l'eau</h1>
+                        <h3>Des forages d'eau de qualité supérieure pour un accès ininterrompu à l'eau potable</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="slide_image">
+                    <img src="image/slide2.jpg" alt="">
+                </div>
+                <div class="slide_filter">
+                    <div class="slide-text">
+                        <h1>Un accès sûr à l'eau</h1>
+                        <h3>Des forages d'eau de qualité supérieure pour un accès ininterrompu à l'eau potable</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- If we need pagination -->
+        <!-- <div class="swiper-pagination"></div> -->
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+        <!-- <div class="swiper-scrollbar"></div> -->
+    </div>
+
+</div>
+
+<div class="section_presentation flex space2 space_bottom">
+    <div class="section_presentation_left">
+        <div class="section_presnt_lef_title">
+            <p>
+                Qui sommes-nous?
+            </p>
+        </div>
+        <div class="section_presentation_left_description">
+            <p>COTECHCOM est une société spécialisée dans la Communication, la Technologie et la construction des
+                forages. Engagée dans une dynamique de croissance, elle développe les segments des particuliers et des
+                professionnels, tout en consolidant ses relations avec les entreprises.</p>
+        </div>
+        <div class="button">
+            <p>En savoir plus</p>
+        </div>
+    </div>
+    <div class="section_presentation_right">
+        <img src="image/who_are_we.png" alt="">
+    </div>
+</div>
+
+<div class="service flex space_interne">
+    <div class="service_left_right space2 flex">
+        <div class="section_presentation_left service_left wow animate__animated animate__fadeInLeft animated">
+            <div class="section_presnt_lef_title service_left_title">
+                <p>
+                    Nos services
+                </p>
+            </div>
+            <div class="section_presentation_left_description service_present_left">
+                <p>COTECHCOM est une société spécialisée dans la Communication, la Technologie et la construction des
+                    forages.</p>
+            </div>
+            <div class="button2 button">
+                <p>Tous les services</p>
+            </div>
+        </div>
+        <div class="section_presentation_right service_right">
+            <div class="all_services flex">
+                <div class="wow animate__animated animate__fadeInRight animated services_item flex">
+                    <div class="flip-card flex">
+                        <div class="flip-card-inner flex">
+                            <div class="flip-card-front flex">
+                                <div class="flip-card-front-image flex">
+                                    <img src="image/icone_btp.svg" alt="Avatar">
+                                    <p>Bâtiments & Travaux Publics</p>
+                                </div>
+
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="flip-card-back-filter">
+                                    <div class="flip-card-back-text">
+                                        <p>Bâtiments & Travaux Publics</p>
+                                    </div>
+                                </div>
+                                <img src="image/service_btp.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card flex">
+                        <div class="flip-card-inner flex">
+                            <div class="flip-card-front flex">
+                                <div class="flip-card-front-image flex">
+                                    <img src="image/icone_btp.svg" alt="Avatar">
+                                    <p>Bâtiments & Travaux Publics</p>
+                                </div>
+
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="flip-card-back-filter">
+                                    <div class="flip-card-back-text">
+                                        <p>Bâtiments & Travaux Publics</p>
+                                    </div>
+                                </div>
+                                <img src="image/service_btp.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card flex">
+                        <div class="flip-card-inner flex">
+                            <div class="flip-card-front flex">
+                                <div class="flip-card-front-image flex">
+                                    <img src="image/icone_btp.svg" alt="Avatar">
+                                    <p>Bâtiments & Travaux Publics</p>
+                                </div>
+
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="flip-card-back-filter">
+                                    <div class="flip-card-back-text">
+                                        <p>Bâtiments & Travaux Publics</p>
+                                    </div>
+                                </div>
+                                <img src="image/service_btp.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card flex">
+                        <div class="flip-card-inner flex">
+                            <div class="flip-card-front flex">
+                                <div class="flip-card-front-image flex">
+                                    <img src="image/icone_btp.svg" alt="Avatar">
+                                    <p>Bâtiments & Travaux Publics</p>
+                                </div>
+
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="flip-card-back-filter">
+                                    <div class="flip-card-back-text">
+                                        <p>Bâtiments & Travaux Publics</p>
+                                    </div>
+                                </div>
+                                <img src="image/service_btp.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card flex">
+                        <div class="flip-card-inner flex">
+                            <div class="flip-card-front flex">
+                                <div class="flip-card-front-image flex">
+                                    <img src="image/icone_btp.svg" alt="Avatar">
+                                    <p>Bâtiments & Travaux Publics</p>
+                                </div>
+
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="flip-card-back-filter">
+                                    <div class="flip-card-back-text">
+                                        <p>Bâtiments & Travaux Publics</p>
+                                    </div>
+                                </div>
+                                <img src="image/service_btp.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card flex">
+                        <div class="flip-card-inner flex">
+                            <div class="flip-card-front flex">
+                                <div class="flip-card-front-image flex">
+                                    <img src="image/icone_btp.svg" alt="Avatar">
+                                    <p>Bâtiments & Travaux Publics</p>
+                                </div>
+
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="flip-card-back-filter">
+                                    <div class="flip-card-back-text">
+                                        <p>Bâtiments & Travaux Publics</p>
+                                    </div>
+                                </div>
+                                <img src="image/service_btp.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
